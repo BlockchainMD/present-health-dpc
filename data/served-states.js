3dc5c284818b4b772cccd6ff43561ee9efe8fbfc
Jon Rouwhorst <jonrouwhorst@gmail.com>
2026-02-11 16:20:54 -0500

    chore(autosync): checkpoint 2026-02-11 16:20:54

--- data/served-states.js
+++ data/served-states.js
@@ -7,7 +7,20 @@
  */
 module.exports = {
     servedStates: [
-        // { name: "Michigan", slug: "michigan" },
+        { name: "Arizona", slug: "arizona" },
+        { name: "Florida", slug: "florida" },
+        { name: "Indiana", slug: "indiana" },
+        { name: "Kentucky", slug: "kentucky" },
+        { name: "Michigan", slug: "michigan" },
+        { name: "Minnesota", slug: "minnesota" },
+        { name: "North Carolina", slug: "north-carolina" },
+        { name: "Nebraska", slug: "nebraska" },
+        { name: "New Hampshire", slug: "new-hampshire" },
+        { name: "Ohio", slug: "ohio" },
+        { name: "Rhode Island", slug: "rhode-island" },
+        { name: "Texas", slug: "texas" },
+        { name: "Utah", slug: "utah" },
+        { name: "Washington", slug: "washington" },
+        { name: "Wisconsin", slug: "wisconsin" },
     ],
 };
-
