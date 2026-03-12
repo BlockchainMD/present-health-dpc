3cd6855c611a20bb1e88845daf307dc905d7365b
Jon Rouwhorst <jonrouwhorst@gmail.com>
2026-03-12 10:03:11 -0400

    chore(autosync): checkpoint 2026-03-12 10:03:10

--- app/admin/healthbook/page.tsx
+++ app/admin/healthbook/page.tsx
@@ -2,6 +2,7 @@ import { HealthbookSeoWorkspace } from "@/components/admin/HealthbookSeoWorkspac
 import { getHealthbookFeedSnapshot } from "@/lib/healthbook";
 
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 export default async function AdminHealthbookPage() {
   const { items, generatedAt } = await getHealthbookFeedSnapshot();
